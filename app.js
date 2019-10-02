abdeb80aa6643595c1b5f23c1610f814fb84eae0
Fernando Matzdorf <fernando@rindecuentas.org>
2019-10-02 13:50:23 -0500

    Added functions for generating simpleName and checking whether a string isCompany.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
 const clean = require('./lib/laundry');
+const simple = require('./lib/simple');
+const isCompany = require('./lib/company');
 
-module.exports = clean;
+module.exports = { launder: clean, simpleName: simple, isCompany: isCompany };
