aa15e0d7e094c2c50e39592d58d669c76f35da8a
Fernando Matzdorf <fernando@rindecuentas.org>
2020-04-01 15:17:40 -0600

    Added function to obtain clean country code from a country name.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const clean = require('./lib/laundry');
 const simple = require('./lib/simple');
 const isCompany = require('./lib/company');
+const cleanCountry = require('./lib/countries');
 
-module.exports = { launder: clean, simpleName: simple, isCompany: isCompany };
+module.exports = { launder: clean, simpleName: simple, isCompany: isCompany, cleanCountry: cleanCountry };
