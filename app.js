736b8ff264be1acb42174f5d6411cf216d74c82b
Fernando Matzdorf <fernando@rindecuentas.org>
2020-06-09 17:19:22 -0500

    Updates and fixes: - Added function companyType to determine if a company is for profit or non-profit - Added various new cases to isCompany

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const clean = require('./lib/laundry');
 const simple = require('./lib/simple');
 const isCompany = require('./lib/company');
+const companyType = require('./lib/companyType');
 const cleanCountry = require('./lib/countries');
 
-module.exports = { launder: clean, simpleName: simple, isCompany: isCompany, cleanCountry: cleanCountry };
+module.exports = { launder: clean, simpleName: simple, isCompany: isCompany, companyType: companyType, cleanCountry: cleanCountry };
